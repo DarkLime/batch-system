d6e87a5228ce6b5281ace7b4400a3115ae245b56
Cyanic <hi.plexify@null.net>
2020-07-14 18:41:29 +0200

    Create Calculator.bat

---
+++ files/Calculator.bat
@@ -0,0 +1,29 @@
+title Calculator
+rem Starting
+echo off
+goto main
+
+:main
+cls
+echo Calculator
+echo.
+set /p sum=
+set /a ans=%sum%
+echo.
+echo = %ans%
+echo.
+echo 1- Another calculation
+echo 2- Start Menu
+choice /c 12 /n
+set c=%ERRORLEVEL%
+if %c% equ 1 goto main
+if %c% equ 2 goto exit1
+
+:exit1
+cls
+echo Exiting
+rem ID=1
+rem Reason=Switching to another program
+cd ..
+BatchSystem.bat
+exit
