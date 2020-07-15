216da214af3253423944438fcc7f4a461fdf67e9
Cyanic <hi.plexify@null.net>
2020-07-15 13:10:17 +0200

    Update FileExplorer.bat

--- files/FileExplorer.bat
+++ files/FileExplorer.bat
@@ -10,8 +10,8 @@ cls
 dir
 echo.
 echo 1-Open file / 2-Open folder / 3-Delete file / 4-Back / 5-Exit
-echo 6-Tree / 7-Delete folder
-choice /c 1234567 /n
+echo 6-Tree / 7-Delete folder / 8-Add folder / 9-Other drives
+choice /c 123456789 /n
 set a=%ERRORLEVEL%
 if %a% equ 1 goto openfile
 if %a% equ 2 goto openfolder
@@ -20,6 +20,8 @@ if %a% equ 4 goto back
 if %a% equ 5 goto exit1
 if %a% equ 6 goto treefarm
 if %a% equ 7 goto rmfolder
+if %a% equ 8 goto mkfolder
+if %a% equ 9 goto letters
 echo Bad choice!
 pause
 goto fx
@@ -74,4 +76,24 @@ dir
 echo.
 set /p folder="Folder to delete: "
 rmdir %folder%
+goto fx
+
+:mkfolder
+cls
+dir
+echo.
+set /p folder="Folder name: "
+mkdir %folder%
+goto fx
+
+:letters
+cls
+echo Choose one of the available drives.
+echo.
+fsutil fsinfo drives
+echo.
+set /p drive="Letter: "
+echo.
+echo Switching to %drive%:...
+%drive%:
 goto fx
