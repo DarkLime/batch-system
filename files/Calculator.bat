4bcacece75091216960992c6c6f44afe4637e101
DarkLime <49911629+DarkLime@users.noreply.github.com>
2020-07-28 08:53:39 +0200

    Add files via upload

--- files/Calculator.bat
+++ files/Calculator.bat
@@ -1,30 +1,23 @@
 title Calculator
 rem Starting
 echo off
-mode con cols=35 lines=11
+mode con cols=25 lines=8
+cls
+echo #======Calculator======#
+echo : press Ctrl+c to exit :
+echo #======================#
+echo.
+
 goto main
 
 :main
-cls
-echo Calculator
-echo.
 set /p sum=
 set /a ans=%sum%
-echo.
-echo = %ans%
-echo.
-echo 1- Another calculation
-echo 2- Start Menu
-choice /c 12 /n
-set c=%ERRORLEVEL%
-if %c% equ 1 goto main
-if %c% equ 2 goto exit1
-
-:exit1
 cls
-echo Exiting
-rem ID=1
-rem Reason=Switching to another program
-cd ..
-BatchSystem.bat
-exit
+echo #======Calculator======#
+echo : press Ctrl+c to exit :
+echo #======================#
+echo.
+echo %sum% = %ans%
+echo.
+goto main
